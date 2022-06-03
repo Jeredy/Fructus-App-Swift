194ebb130bf758ef607d09b8991eae873b4c3228
Andre Rodrigues <andrecomerciale@gmail.com>
2022-06-03 13:12:10 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by André Almeida on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
